81afa6734fe4cf83713ae21aab4af06c61b3158b
TeoGiane <gianeteo@hotmail.it>
2026-07-21 09:51:47 +0800

    Add wrap_multiview_output() helping function

--- src/mcmc_utils.h
+++ src/mcmc_utils.h
@@ -3,6 +3,7 @@
 #include <random>
 
 #include <RcppArmadillo.h>
+#include "multiview_voronoi_sampler.h"
 
 // Sample from Truncated Normal N(mu, sigma) on [a, b]
 double truncated_normal_rng(double mean, double sd, double min, double max, std::mt19937 & rng);
@@ -15,3 +16,6 @@ double beta_lpdf(double x, double alpha, double beta);
 
 // Log-PDF of a Gamma Distribution
 double gamma_lpdf(double x, double shape, double rate);
+
+// Wrap the output of a multiview sampler into a structured Rcpp list
+Rcpp::List wrap_multiview_output(const MultiViewMCMCOutput& out);
